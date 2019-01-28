0e30cf1506250ee122e4f608f1c83a6c00d69d99
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-01-28 07:18:55 -0200

    Update checkswapfile.sh

--- checkswapfile.sh
+++ checkswapfile.sh
@@ -6,3 +6,10 @@ fdisk -l /dev/sdc
 mkswap /dev/sdc1
 swapon /dev/sdc1
 swapon -s
+gedit /etc/fstab
+/swapfile   none    swap    sw    0   0
+cat /proc/sys/vm/swappiness
+sudo gedit /etc/sysctl.conf
+vm.swappiness = 40
+
+
