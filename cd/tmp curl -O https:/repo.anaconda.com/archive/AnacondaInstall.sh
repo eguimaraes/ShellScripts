d8aecc976979a7327a94fd4feb8d3f554b2bb7da
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-12 14:46:19 -0200

    Delete AnacondaInstall.sh

--- cd/tmp curl -O https:/repo.anaconda.com/archive/AnacondaInstall.sh
+++
@@ -1,8 +0,0 @@
-cd /tmp
-curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
-sha256sum Anaconda3-5.2.0-Linux-x86_64.sh
-bash Anaconda3-5.2.0-Linux-x86_64.sh
-source ~/.bashrc
-conda list
-conda create --name my_env python=3
-source activate my_env
