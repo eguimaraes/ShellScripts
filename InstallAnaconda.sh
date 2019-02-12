ada7ecd5f2069f85ad677aafd25606b430a08e65
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-12 21:08:02 -0200

    Update InstallAnaconda.sh

--- InstallAnaconda.sh
+++ InstallAnaconda.sh
@@ -5,7 +5,15 @@ bash Anaconda3-5.2.0-Linux-x86_64.sh
 source ~/.bashrc
 conda list
 conda create --name aprendendo python=3
+#Ativar Ambiente
 source activate aprendendo
+conda create --name aprendendoP2 python=2
+source activate aprendendoP2
 conda install --name aprendendo scipy
-conda install -c openastronomy healpy 
-conda install -c anaconda hdf5 
+conda install --name aprendendo -c openastronomy healpy 
+conda install --name aprendendo -c anaconda hdf5 
+conda install --name aprendendo -c anaconda astr
+#Desativar Ambiente
+source deactivate aprendendo
+source deactivate aprendendoP2
+
