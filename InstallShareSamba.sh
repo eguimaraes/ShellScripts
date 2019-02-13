7a9237479f8a82f23a9de8506b952905462d9a9d
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-13 11:07:43 -0200

    Update and rename InstaçllShareSamba.sh to InstallShareSamba.sh

--- InstallShareSamba.sh
+++ InstallShareSamba.sh
@@ -1,4 +1,5 @@
-#https://itsfoss.com/share-folders-local-network-ubuntu-windows/sudo apt-get install nautilus-share
+#https://itsfoss.com/share-folders-local-network-ubuntu-windows/
+sudo apt-get install nautilus-share
 nautilus -q
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install samba samba-common system-config-samba python-glade2 gksu
