ac9d9aed6146c7202a99a34f37a473b8a235d7a2
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-13 11:26:33 -0200

    Update InstallShareSamba.sh

--- InstallShareSamba.sh
+++ InstallShareSamba.sh
@@ -2,4 +2,7 @@
 sudo apt-get install nautilus-share
 nautilus -q
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install samba samba-common system-config-samba python-glade2 gksu
+sudo apt-get install python-glade2
+sudo apt-get install samba samba-common system-config-samba python-glade2
+adduser yourname
+passwd yourname
