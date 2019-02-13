f09d430c6dc3333b9e4d1ae4f41835b0a43eb264
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-13 11:06:45 -0200

    Create InstaçllShareSamba.sh

---
+++ InstaçllShareSamba.sh
@@ -0,0 +1,4 @@
+#https://itsfoss.com/share-folders-local-network-ubuntu-windows/sudo apt-get install nautilus-share
+nautilus -q
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install samba samba-common system-config-samba python-glade2 gksu
