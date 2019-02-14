06d5ab8cf77a0dbb32ac6a14d40cb3cbf1e6f932
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-14 10:33:19 -0200

    Create InstallTelnet.sh

---
+++ InstallTelnet.sh
@@ -0,0 +1,6 @@
+sudo apt-get install xinetd telnetd
+sudo apt-get update && apt-get install telnet
+sudo apt-get install openssh-server
+sudo service ssh status
+sudo gedit /etc/ssh/sshd_config
+sudo service ssh restart
