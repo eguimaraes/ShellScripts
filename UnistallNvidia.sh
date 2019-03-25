e37cf36fb7eb129a46cf34e5e8ef2a46f34f9542
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-03-25 07:55:06 -0300

    Create UnistallNvidia.sh

---
+++ UnistallNvidia.sh
@@ -0,0 +1,5 @@
+sudo apt-get purge nvidia-*
+sudo apt-add-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+sudo apt-cache search '^nvidia-[:digit]' --names-only
+sudo apt-get install nvidia-381
