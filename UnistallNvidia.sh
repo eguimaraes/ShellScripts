caaf3ba31ed896a4dabac92f026e732c07649914
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-04-03 13:25:29 -0300

    Update UnistallNvidia.sh

--- UnistallNvidia.sh
+++ UnistallNvidia.sh
@@ -2,4 +2,4 @@ sudo apt-get purge nvidia-*
 sudo apt-add-repository ppa:graphics-drivers/ppa
 sudo apt-get update
 sudo apt-cache search '^nvidia-[:digit]' --names-only
-sudo apt-get install nvidia-418
+sudo apt-get install nvidia-390
