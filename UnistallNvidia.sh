bc7eeffa662631ebfc496d2d17003557459dd7a0
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-04-04 19:09:53 -0300

    Update UnistallNvidia.sh

--- UnistallNvidia.sh
+++ UnistallNvidia.sh
@@ -1,3 +1,8 @@
+#https://askubuntu.com/questions/61396/how-do-i-install-the-nvidia-drivers
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt upgrade
+ubuntu-drivers list
 sudo apt-get purge nvidia-*
 sudo apt-add-repository ppa:graphics-drivers/ppa
 sudo apt-get update
