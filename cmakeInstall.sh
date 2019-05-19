bd829eb5433ef833aee4b38798e008d30cb4f897
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-05-19 17:36:41 -0300

    Update cmakeInstall.sh

--- cmakeInstall.sh
+++ cmakeInstall.sh
@@ -5,7 +5,7 @@ build=4
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
-./bootstrap
+sudo ./bootstrap
 make -j4
 sudo make install
 cmake --version
