53515744a349680a12b67b11207923b89c458d94
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-05-19 16:22:45 -0300

    Update util.sh

--- util.sh
+++ util.sh
@@ -28,3 +28,15 @@ sudo apt-get remove virtualbox* --purge
 InstallAnaconda.sh
 #dism.exe /Online /Disable-Feature:Microsoft-Hyper-V
 sudo apt-get install gnome-system-tools
+#atualizar o cmake
+sudo apt-get purge cmake
+sudo apt-get install cmake
+sudo apt-get purge cmake
+version=3.14
+build=4
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+./bootstrap
+make -j4
+sudo make install
