41f34d3bdfd66f55adaca1091da2728a8b4298b5
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-05-19 16:44:03 -0300

    Update cmakeInstall.sh

--- cmakeInstall.sh
+++ cmakeInstall.sh
@@ -1,7 +1,5 @@
 #atualizar o cmake
 sudo apt-get purge cmake
-sudo apt-get install cmake
-sudo apt-get purge cmake
 version=3.14
 build=4
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
@@ -10,3 +8,4 @@ cd cmake-$version.$build/
 ./bootstrap
 make -j4
 sudo make install
+cmake --version
