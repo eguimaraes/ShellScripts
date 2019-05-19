2b4e119fdc73ab6e75ddb9b883f69b31d2ffb43f
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-05-19 16:23:08 -0300

    Create cmakeInstall.sh

---
+++ cmakeInstall.sh
@@ -0,0 +1,12 @@
+#atualizar o cmake
+sudo apt-get purge cmake
+sudo apt-get install cmake
+sudo apt-get purge cmake
+version=3.14
+build=4
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+./bootstrap
+make -j4
+sudo make install
