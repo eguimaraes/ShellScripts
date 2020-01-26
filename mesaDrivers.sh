4e74fb359f30fccdab2c634124e9d8fe977f9790
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-01-26 17:19:08 -0300

    Update mesaDrivers.sh

--- mesaDrivers.sh
+++ mesaDrivers.sh
@@ -2,6 +2,7 @@ sudo glxinfo | grep "OpenGL version"
 glxinfo | grep '^direct rendering:'
 lspci | grep VGA
 sudo add-apt-repository ppa:ubuntu-x-swat/updates
+sudo add-apt-repository ppa:oibaf/graphics-drivers
 sudo apt-get dist-upgrade
 glxinfo | grep "OpenGL version"
 sudo apt-get install ppa-purge && sudo ppa-purge ppa:ubuntu-x-swat/updates
