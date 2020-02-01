c8b692da33457730c5756d5d0022aead8128e043
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-02-01 20:55:22 -0300

    Update installUCLCL.sh

--- installUCLCL.sh
+++ installUCLCL.sh
@@ -1,1 +1,12 @@
 sudo apt-get purge cmake
+sudo apt-get install OpenSSL*
+sudo apt-get install libssl-dev
+#download and compile CMAKE lastest version at https://cmake.org/download/ - download source
+cd diretoriofonte cmake
+./bootstrap && make && sudo make install
+sudo cp /usr/local/include/ /usr/include/ -r
+sudo cp /usr/local/lib/ /usr/lib/ -r
+sudo cp /usr/local/bin/ /usr/bin/ -r
+sudo apt-get install libboost-all-dev
+
+
