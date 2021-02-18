45f081aa02e065a6b6a92ed4df226f66d98681aa
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-18 08:16:59 -0300

    Create installPip2.sh

---
+++ installPip2.sh
@@ -0,0 +1,6 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo apt update 
+sudo apt install python2
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+python2 get-pip.py
