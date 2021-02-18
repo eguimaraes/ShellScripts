0002c1bc5bb0139f94e6c53b641eaf5435758d47
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-02-18 08:22:18 -0300

    Create listDistroName.sh
    List Linux Distro Name

---
+++ listDistroName.sh
@@ -0,0 +1,17 @@
+lsb_release -a
+cat /etc/os-release
+lsb_release -a
+cat /etc/issue
+cat /etc/lsb-release
+cat /etc/os-release
+cat /etc/centos-release
+cat /etc/lsb-release
+cat /etc/redhat-release
+cat /etc/system-release
+cat /etc/os-release
+cat /proc/version
+uname -a
+cat /etc/[A-Za-z]*[_-][rv]e[lr]*
+lsb_release -a || cat /etc/redhat-release || cat /etc/*-release || cat /etc/issue
+cat /etc/*-release | uniq -u
+ 
