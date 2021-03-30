73e5831959410c0cac86dde4f144cf0ee391f186
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-03-30 19:58:41 -0300

    Create InstallKobi.sh

---
+++ InstallKobi.sh
@@ -0,0 +1,8 @@
+#https://pimylifeup.com/raspberry-pi-netflix/
+sudo apt update
+sudo apt upgrade
+sudo apt install kodi kodi-peripheral-joystick kodi-pvr-iptvsimple kodi-inputstream-adaptive kodi-inputstream-rtmp
+sudo apt install build-essential python-pip python-dev libffi-dev libssl-dev libnss3
+sudo pip install setuptools wheel
+sudo pip install pycryptodomex win_inet_pton
+wget https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.1.zip
