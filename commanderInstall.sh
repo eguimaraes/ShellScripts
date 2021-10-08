1bcf15bbb84479644a85259cd92a28e5c95793bb
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-10-08 10:06:12 -0300

    Update commanderInstall.sh

--- commanderInstall.sh
+++ commanderInstall.sh
@@ -2,4 +2,4 @@ git clone https://github.com/hke/Commander.git
 cd Commander
 mkdir build
 cd build
-cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_Fortran_COMPILER=ifort
+cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_Fortran_COMPILER=ifort -S $PWD -B $PWD/build
