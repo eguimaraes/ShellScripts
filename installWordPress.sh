2b17e64b67c321e7789c7c987e124e89427b3fae
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2023-04-28 09:39:44 -0300

    Create installWordPress.sh
    https://ubuntu.com/tutorials/install-and-configure-wordpress#3-install-wordpress

---
+++ installWordPress.sh
@@ -0,0 +1,19 @@
+#https://ubuntu.com/tutorials/install-and-configure-wordpress#3-install-wordpress
+sudo apt update
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip
+                 sudo mkdir -p /srv/www
+sudo chown www-data: /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
