4b10560e8cc80c46c9c56be5f568411bc2996d81
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2023-07-18 14:21:57 -0300

    Create addSefSignedRootCert.ps1

---
+++ addSefSignedRootCert.ps1
@@ -0,0 +1,5 @@
+#https://learn.microsoft.com/en-us/azure/virtual-wan/certificates-point-to-site
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
