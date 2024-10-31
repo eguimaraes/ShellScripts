0628217de5cfdb77f653285c833e87f1d75a0003
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2024-10-30 21:04:19 -0300

    Create GitCriarRepo.sh

---
+++ GitCriarRepo.sh
@@ -0,0 +1,12 @@
+echo "# EkisDataAnalises" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/eguimaraes/EkisDataAnalises.git
+git push -u origin main
+
+
+git remote add origin https://github.com/eguimaraes/EkisDataAnalises.git
+git branch -M main
+git push -u origin main
